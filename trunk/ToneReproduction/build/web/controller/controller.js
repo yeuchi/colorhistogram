2dff4935f52c854a2f88303291d5c737eafd8f38
yeuchi@gmail.com <yeuchi@gmail.com@3895b39a-ff46-11dd-87c9-111d11f6bf44>
2012-07-31 02:10:47 +0000

    1) build histogram 2) render histogram.

--- trunk/ToneReproduction/build/web/controller/controller.js
+++ trunk/ToneReproduction/build/web/controller/controller.js
@@ -12,5 +12,15 @@ Controller.prototype.init = function() {
 }
 
 Controller.prototype.buildHistogram = function() {
+   model.initHistogram();
    
+   for (var y=0; y<model.imageHeight; y++) {
+		var i = y*model.imageWidth*4;			
+		for(var x=0; x<model.imageWidth; x++) {
+         model.histogramR[model.dataSrc.data[i]];        // R
+         model.histogramG[model.dataSrc.data[++i]];		// G
+         model.histogramB[model.dataSrc.data[++i]];		// B
+         model.histogramA[model.dataSrc.data[++i]];		// A
+      }
+   }
 }
--- trunk/ToneReproduction/build/web/view/histogramGraph/histogram.js
+++ trunk/ToneReproduction/build/web/view/histogramGraph/histogram.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
    
    var barGraph = new BarGraph();
    var dlgControl = new DialogControl();
+   
+   barGraph.render();                     // draw histogram
 });
 
 
