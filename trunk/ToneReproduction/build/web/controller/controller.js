d0787141b00e4dc1239c85a2fe55ce3a0fc64b94
yeuchi@gmail.com <yeuchi@gmail.com@3895b39a-ff46-11dd-87c9-111d11f6bf44>
2012-07-31 21:56:09 +0000

    adding histogram calculation with statistics.

--- trunk/ToneReproduction/build/web/controller/controller.js
+++ trunk/ToneReproduction/build/web/controller/controller.js
@@ -4,22 +4,72 @@
  */
 
 var Controller = function() {
-   
+   this.init();
 }
 
 Controller.prototype.init = function() {
-   
+   this.modelEnum = new ModelEnum();
 }
 
 Controller.prototype.buildHistogram = function() {
+   // create a local copy of use global copy in model ?
    model.initHistogram();
-   for (var y=0; y<model.imageHeight; y++) {
-		var i = y*model.imageWidth*4;			
-		for(var x=0; x<model.imageWidth; x++) {
-         model.histogramR[model.dataSrc.data[i]];        // R
-         model.histogramG[model.dataSrc.data[++i]];		// G
-         model.histogramB[model.dataSrc.data[++i]];		// B
-         model.histogramA[model.dataSrc.data[++i]];		// A
+   
+   for (var y=0; y<model.imageHeight(); y++) {
+		var i = y*model.imageWidth()*4;			
+		for(var x=0; x<model.imageWidth(); x++) {
+         for(var c=0; c<this.modelEnum.NUM_CHANNELS; c++)
+            this.upCount(c, model.listInfo[c]);
+      }
+   }
+
+   // get max and min
+   for(var k=0; k<this.modelEnum.NUM_CHANNELS; k++){
+      this.findShadow(k, model.listInfo[k]);
+      this.findHighlight(k, model.listInfo[k]);
+      this.findStat(k, model.listInfo[k]);
+   }
+   model.changeState(this.modelEnum.STATE_HISTOGRAM); 
+}
+
+Controller.prototype.upCount = function(index, info) {
+   var value = model.dataSrc.data[index];
+   info.histogram[value]++;
+}
+
+Controller.prototype.findShadow = function(index, info) {
+   for(var m=0; m<this.modelEnum.HISTOGRAM_LENGTH; m++) {
+      if(info.histogram[m]>0) {
+         info.shadow = m;
+         return;
+      }
+   }
+}
+
+Controller.prototype.findHighlight = function(index, info) {
+   for(var m=this.modelEnum.HISTOGRAM_LENGTH-1; m>=0; m--) {
+      if(info.histogram[m]>0) {
+         info.highlight = m;
+         return;
+      }
+   }
+}
+
+Controller.prototype.findStat = function(index, info) {
+   var medianPos = model.imageWidth * model.imageHeight / 2;
+   var count = 0;
+   
+   for(var m=0; m<this.modelEnum.HISTOGRAM_LENGTH; m++) {
+      
+      // median
+      count += info.histogram[m];
+      if(count>medianPos)
+         info.median = m;
+      
+      // mode, max
+      if(info.histogram[m]>info.max) {
+         info.mode = m;
+         info.max = info.histogram[m];
       }
    }
 }
--- trunk/ToneReproduction/web/model/model.js
+++ trunk/ToneReproduction/web/model/model.js
@@ -8,18 +8,27 @@ var Model = function() {
    this._modelEnum = new ModelEnum();
    this.imageUrl;
    this.state = this._modelEnum.STATE_INIT;
-   
-   this.modelEnum = new ModelEnum();
+   this.listInfo = [];
    this.initHistogram();
    
    return(model)?model:model=this;
 }
 
 Model.prototype.initHistogram = function() {
-   this.histogramR = [];                        // array of 256 entries (8 bit channel)
-   this.histogramG = [];
-   this.histogramB = [];
-   this.histogramA = [];  
+   this.listInfo = [];
+   var red = new ChannelInfo(this._modelEnum.CHANNEL_RED);
+   this.listInfo.push(red);
+   this.listInfo.push(new ChannelInfo(this._modelEnum.CHANNEL_GREEN));
+   this.listInfo.push(new ChannelInfo(this._modelEnum.CHANNEL_BLUE));
+   this.listInfo.push(new ChannelInfo(this._modelEnum.CHANNEL_ALPHA));
+}
+
+Model.prototype.getChannelInfo = function(color) {
+   for(var i=0; i<4; i++) {
+      if(this.listInfo[i].channel==color)
+         return this.listInfo[i];
+   }
+   return null;
 }
 
 // application state change
@@ -27,17 +36,18 @@ Model.prototype.changeState = function() {
    appView.onStateChange();                     // replace with function binding if observer pattern
 }
 
-Model.prototype.initImage = function(id) {
+Model.prototype.initImage = function(id, imgObj) {
    this.canvas = document.getElementById(id);
    this.context = this.canvas.getContext("2d");
+   this.context.drawImage(imgObj, 0,0);
    this.dataSrc = this.context.getImageData(0,0,this.canvas.width, this.canvas.height);
    this.dataDes = this.context.createImageData(this.canvas.width, this.canvas.height);
 }
 
 Model.prototype.imageWidth = function() {
-   return (null==this.imgSrc)?-1:this.imgSrc.width;
+   return (null==this.dataSrc)?-1:this.dataSrc.width;
 }
 
 Model.prototype.imageHeight = function() {
-   return (null==this.imgSrc)?-1:this.imgSrc.height;
+   return (null==this.dataSrc)?-1:this.dataSrc.height;
 }
